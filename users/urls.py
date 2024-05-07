74fd65ad04a59e4cf4ad0fdfb170c29a32bef57f
clinton-CG <166376474+clinton-CG@users.noreply.github.com>
2024-05-07 17:10:31 +0300

    password recovery

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,20 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from django.contrib.auth.views import (
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
+
 urlpatterns = [
     path('login/', views.login_view,name="login"),
     path('signup/', views.signup_view,name="signup"),
-    path('logout/', views.logout_view,name="logout")
+    path('logout/', views.logout_view,name="logout"),
+    path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset.html'),name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('password-reset-complete/',PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
 
 ]
--- youtify/settings.py
+++ youtify/settings.py
@@ -127,3 +127,18 @@ STATICFILES_DIRS = [
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = 'login'
+
+
+
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'myemail'
+EMAIL_HOST_PASSWORD = 'mypassword'
+
+DEFAULT_FROM_EMAIL = 'myemail'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
